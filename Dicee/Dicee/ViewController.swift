b807afb656168bbe28939676abbb1635c78f9020
hoffmantim <hoffmant@hilbertk12.org>
2018-04-30 15:42:19 -0500

    Finished app: created array of images, variable for random number, wrote func to change diceImage & pick new image from array using random number. Still need to fix constraints and sizing of UI elements for different screen sizes.

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    let diceImageArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func rollButtonWasPressed(_ sender: Any) {
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        diceImageView1.image = UIImage(named: diceImageArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceImageArray[randomDiceIndex2])
+        
+    }
+    
 }
 
